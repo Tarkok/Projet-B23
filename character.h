9e0800bce60ad4e6a967dee3f6aa55ae9ecca190
Théo <conoracre62@gmail.com>
2018-01-08 16:37:48 +0100

    s

--- character.h
+++ character.h
@@ -3,8 +3,8 @@
 
 typedef struct {
 
-	Point2D positionTete;
-	int length;
-	int peutTraverser;
+	Point2D positionTete; // Position de la tete du serpent
+	int length;			// Taille de la queue du serpent (sans la tete)
+	int peutTraverser; //  Booléen, indique si l'ont peut traverser les murs
 
 }Character;
--- map.h
+++ map.h
@@ -1,9 +1,21 @@
 #pragma once
+#include "character.h"
+#include "outilMath.h"
+
+typedef struct {
+
+	Point2D pos; //Position d'un objet
+	int canKill; //Booléen 0 = mur, 1 = peutTue = piege
+
+}Placable;
 
 typedef struct {
 
 	unsigned int width = 45;
 	unsigned int height = 20;
 	unsigned int carte[45][20];
+
+	Character Snake;
+
 
 }Map;
