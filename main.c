bf4bb4d81aa51f36fd69324587570d7eb6192521
Théo <conoracre62@gmail.com>
2018-01-09 09:16:54 +0100

    Redemmare a 0

--- main.c
+++ main.c
@@ -1,35 +1,67 @@
 #include <stdio.h>
-#include <conio.h>
-#include "outilMath.h"
-#include "map.h"
-#include "character.h"
-#include "gameManager.h"
+#include <stdlib.h>
 
+//Declaration des constantes & variables
+const int WIDTH = 25;
+const HEIGHT = 15;
+
+
+struct Point2D{
+    int x;
+    int y;
+};
+typedef struct Point2D Point2D;
+
+struct Snake{
+    Point2D posTete;
+    Point2D posQueue[];
+};
+typedef struct Snake Snake;
+
+void load(const char* nomFichier, int carte[][HEIGHT])
+{
+   FILE* fichier = fopen(nomFichier, "r");
+
+   if(fichier != NULL)
+   {
+      for(int i = 0; i < HEIGHT; i++)
+        {
+            for(int j = 0; j < WIDTH; j++)
+            {
+                carte[j][i] = getc(fichier);
+            }
+        }
+
+        fclose(fichier);
+   }
+   else
+   {
+       printf("Impossible de charger le niveau\n");
+   }
+}
+
+void draw(int carte[][HEIGHT])
+{
+      for(int i = 0; i < HEIGHT; i++)
+        {
+            for(int j = 0; j < WIDTH; j++)
+            {
+                printf("%c",carte[j][i]);
+            }
+        }
+        printf("\n");
+
+        draw(*carte);
+}
 
 int main()
 {
-	int i, j;
-	char input;
-	Character Snake;
-	Snake.positionTete.x = 2;
-	Snake.positionTete.y = 2;
-	Map carte;
+    int carte[WIDTH][HEIGHT];
+    /** a)Deplacement manuel du snake*/
+    load("level/level_1_2.txt", *carte);
+    draw(*carte);
 
-	do
-	{
-		input = getch();
-		manageInput(input, Snake);
-		for (i = 0; i < 45; i++)
-		{
-			for (j = 0; j < 20; j++)
-			{
-				printf("%c",carte.carte[i][j]);
-			}
-			printf("\n");
-		}
 
-	} while (input != 'e');
-	
 
-	return 0;
+    return 0;
 }
